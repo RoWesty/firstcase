d593a5d9ca62d525bd26864d4e39ad9bce86996c
RoWesty <132814003+RoWesty@users.noreply.github.com>
2023-06-29 17:46:01 +0300

    Финальная витрина Request.sql

--- Request.sql
+++ Request.sql
@@ -1,19 +1,17 @@
- WITH sl_fact AS (SELECT sd.sales_cnt FROM shop_dns AS sd, (SELECT DATE_PART('month', CURRENT_DATE) AS curmonth, DATE_PART('YEAR', CURRENT_DATE) AS curyear, DATE_PART('month', pl.plan_date) as mont, DATE_PART('YEAR', pl.plan_date) as yea 
-                 FROM plan as pl) as two WHERE yea = curyear AND mont = curmonth group by sd.sales_cnt),
-                (SELECT SUM(sm1) AS sales_fact 
+ WITH sl_fact AS(SELECT SUM(sm1) AS sales_fact 
                  FROM
                 (SELECT SUM(sales_cnt) AS sm1
-                 FROM (SELECT sd.sales_cnt FROM shop_dns AS sd, (SELECT DATE_PART('month', CURRENT_DATE) AS curmonth, DATE_PART('YEAR', CURRENT_DATE) AS curyear, DATE_PART('month', pl.plan_date) as mont, DATE_PART('YEAR', pl.plan_date) as yea 
-                 FROM plan as pl) as two WHERE yea = curyear AND mont = curmonth group by sd.sales_cnt) AS one
+                 FROM shop_dns sd
+                 WHERE DATE_PART('month', sd."date") = ALL(SELECT DATE_PART('month', CURRENT_DATE) FROM plan pl WHERE DATE_PART('month',pl.plan_date) = DATE_PART('month', CURRENT_DATE)) AND DATE_PART('year', sd."date") = ALL(SELECT DATE_PART('year', CURRENT_DATE) FROM plan pl WHERE DATE_PART('year',pl.plan_date) = DATE_PART('year', CURRENT_DATE))
                  UNION ALL
                  SELECT SUM(sales_cnt)
-                 FROM (SELECT sm.sales_cnt FROM shop_mvideo AS sm, (SELECT DATE_PART('month', CURRENT_DATE) AS curmonth, DATE_PART('YEAR', CURRENT_DATE) AS curyear, DATE_PART('month', pl.plan_date) as mont, DATE_PART('YEAR', pl.plan_date) as yea 
-                 FROM plan as pl) as fre WHERE yea = curyear AND mont = curmonth group by sm.sales_cnt) AS four
+                 FROM shop_mvideo sm
+                 WHERE DATE_PART('month', sm."date") = ALL(SELECT DATE_PART('month', CURRENT_DATE) FROM plan pl WHERE DATE_PART('month',pl.plan_date) = DATE_PART('month', CURRENT_DATE)) AND DATE_PART('year', sm."date") = ALL(SELECT DATE_PART('year', CURRENT_DATE) FROM plan pl WHERE DATE_PART('year',pl.plan_date) = DATE_PART('year', CURRENT_DATE))
                  UNION ALL
                  SELECT SUM(sales_cnt)
-                 FROM (SELECT ss.sales_cnt FROM shop_sitilink AS ss, (SELECT DATE_PART('month', CURRENT_DATE) AS curmonth, DATE_PART('YEAR', CURRENT_DATE) AS curyear, DATE_PART('month', pl.plan_date) as mont, DATE_PART('YEAR', pl.plan_date) as yea 
-                 FROM plan as pl) as five WHERE yea = curyear AND mont = curmonth group by ss.sales_cnt ) AS six) AS sumall),
-    sl_plan AS (SELECT SUM(pl.plan_cnt) AS sales_plan 
+                 FROM shop_sitilink ss
+                 WHERE DATE_PART('month', ss."date") = ALL(SELECT DATE_PART('month', CURRENT_DATE) FROM plan pl WHERE DATE_PART('month',pl.plan_date) = DATE_PART('month', CURRENT_DATE)) AND DATE_PART('year', ss."date") = ALL(SELECT DATE_PART('year', CURRENT_DATE) FROM plan pl WHERE DATE_PART('year',pl.plan_date) = DATE_PART('year', CURRENT_DATE))) as sum_fact),
+    sl_plan AS (SELECT SUM(pl.plan_cnt) AS sales_plan
                  FROM plan pl
                  group by pl.plan_date),
    sl_ratio AS (SELECT sales_fact/sales_plan AS sales_ratio
